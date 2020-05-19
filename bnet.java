b0b8d6e9543e85bc61324f62b6d77986b989f11e
Divya Saxena <54782918+divyasaxenaa@users.noreply.github.com>
2020-05-19 14:03:19 -0400

    Add files via upload

---
+++ bnet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Divya Saxena
+ *
+ */
+public class bnet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BayeisnNetwork bayeisnNetwork = new BayeisnNetwork();
+		bayeisnNetwork.calculateBayeisnNetwork(args);
+	}
+
+}
